b6f41bf2c92bec372360ab84b529043fb84b31e9
manojmanu27-dell <138210050+manojmanu27-dell@users.noreply.github.com>
2024-07-21 08:55:25 +0000

    added backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    api: "http://localhost:8090/",
+    api: "https://trello-clone-api-8rpi.onrender.com/",
     production: true,
     clientId: "674662451750-5rg68qofgki65l332bdtlvi01v64k6k4.apps.googleusercontent.com"
 };
